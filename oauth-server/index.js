0b8da6f0ddf727fd4429ab6a765e40047eef9a41
Nabila Alam <nabilazaheer1198@gmail.com>
2025-06-27 11:48:52 +0530

    feat: GOOGLE OAuth

--- oauth-server/index.js
+++ oauth-server/index.js
@@ -39,6 +39,42 @@ app.get("/auth/github/callback", async (req, res) => {
   }
 });
 
+// GOOGLE OAUTH
+
+app.get("/auth/google", (req, res) => {
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+  res.redirect(googleAuthUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    return res.status(400).send("Authorization code not provided.");
+  }
+  let accessToken;
+
+  try {
+    const tokenResponse = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    accessToken = tokenResponse.data.access_token;
+    res.cookie("access_token", accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on https://localhost: ${PORT}`);
 });
